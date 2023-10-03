b9a2c28405a99ab3862b61ad1848d042b2c63e4b
drshounak <139616389+drshounak@users.noreply.github.com>
2023-10-03 12:25:20 +0530

    Update scripts.js

--- todo/scripts.js
+++ todo/scripts.js
@@ -2,7 +2,7 @@ document.getElementById('login').addEventListener('click', function() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if (username === 'sonu' && password === 'Ici@sidhu90') {
+    if (username === 'Sonu' && password === 'Icisidhu') {
       document.getElementById('login-screen').style.display = 'none';
       document.getElementById('content').style.display = 'block';
     } else {
