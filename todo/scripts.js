3727c9392b57998d6d0c4a44fbb4a0c2cf35ec88
drshounak <139616389+drshounak@users.noreply.github.com>
2023-10-13 15:39:39 +0530

    Update scripts.js

--- todo/scripts.js
+++ todo/scripts.js
@@ -1,29 +1,30 @@
 // Predetermined hashed password (example value)
 var predeterminedHashedPassword = '27335363faec9e8430281a6615cc1a893162459f563f9c8842234783d4daa413'; // Replace this with your actual predetermined hashed password
 
-document.getElementById('login').addEventListener('click', function() {
-    var username = document.getElementById('username').value;
-    var inputPassword = document.getElementById('password').value;
-    var hashedPassword = CryptoJS.SHA256(inputPassword).toString(); // Hash the input password using SHA-256
-
-    // Compare the hashed password with the predetermined hashed password
-    if (username === 'Sonu' && hashedPassword === predeterminedHashedPassword) {
-        // Call the function to handle login and session management
-        handleLogin();
-        
-        // Rest of your login logic
-    } else {
-        alert('Invalid credentials. Please try again.');
-    }
-
-    // Rest of your login logic
-});
-
 document.addEventListener('DOMContentLoaded', function() {
+    // Event listener for the login button
+    document.getElementById('login').addEventListener('click', function() {
+        var username = document.getElementById('username').value;
+        var inputPassword = document.getElementById('password').value;
+        var hashedPassword = CryptoJS.SHA256(inputPassword).toString(); // Hash the input password using SHA-256
+
+        // Compare the hashed password with the predetermined hashed password
+        if (username === 'Sonu' && hashedPassword === predeterminedHashedPassword) {
+            // Call the function to handle login and session management
+            handleLogin();
+
+            // Hide the login screen and show the content
+            document.getElementById('login-screen').style.display = 'none';
+            document.getElementById('content').style.display = 'block';
+        } else {
+            alert('Invalid credentials. Please try again.');
+        }
+    });
+
+    // Check if the user is already logged in and display the appropriate content
     var isLoggedIn = localStorage.getItem('loggedIn');
     var expirationTime = parseInt(localStorage.getItem('expirationDate'));
 
-    // Check if the user is already logged in and the session has not expired
     if (isLoggedIn === 'true' && expirationTime && expirationTime > Date.now()) {
         // User is already logged in and the session is still valid, so display the content
         document.getElementById('login-screen').style.display = 'none';
@@ -63,4 +64,3 @@ function handleLogin() {
 
     // Additional logic for handling the user session can be added here
 }
-
