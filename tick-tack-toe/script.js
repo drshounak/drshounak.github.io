a66034217362723ca18995303eff71135319ae84
drshounak <139616389+drshounak@users.noreply.github.com>
2023-11-09 15:12:48 +0530

    Update script.js

--- tick-tack-toe/script.js
+++ tick-tack-toe/script.js
@@ -22,31 +22,69 @@ function checkWinner() {
     return null;
 }
 
+function checkGameState() {
+    const winner = checkWinner();
+    if (winner !== null) {
+        document.getElementById('message').textContent = `Player ${winner} wins!`;
+    } else if (board.every(square => typeof square !== 'number')) {
+        document.getElementById('message').textContent = "It's a draw!";
+    }
+}
+
 function getAiMove() {
-        let availableSquares = board.filter(s => typeof s === 'number');
-        if (availableSquares.length > 0) {
-            let randomMove = availableSquares[Math.floor(Math.random() * availableSquares.length)];
-            return randomMove;
+    let availableSquares = board.filter(s => typeof s === 'number');
+
+    // Check for a winning move for AI
+    for (let move of availableSquares) {
+        board[move] = currentPlayer;
+        if (checkWinner() === currentPlayer) {
+            board[move] = move; // Undo the move
+            return move;
         }
-        return null;
+        board[move] = move; // Undo the move
     }
 
-    function makeMove(square) {
-        if (typeof board[square] === 'number') {
-            board[square] = currentPlayer;
-            document.getElementById('message').textContent = '';
-            document.querySelectorAll('.cell')[square].textContent = currentPlayer;
-            checkGameState();
+    // Check for a winning move for the user and block it
+    for (let move of availableSquares) {
+        board[move] = currentPlayer === 'X' ? 'O' : 'X';
+        if (checkWinner() === (currentPlayer === 'X' ? 'O' : 'X')) {
+            board[move] = move; // Undo the move
+            return move;
+        }
+        board[move] = move; // Undo the move
+    }
 
-            let aiMove = getAiMove();
-            if (aiMove !== null) {
-                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-                board[aiMove] = currentPlayer;
-                document.querySelectorAll('.cell')[aiMove].textContent = currentPlayer;
-                checkGameState();
-            }
+    // If there are no immediate winning moves, choose a random move
+    if (availableSquares.length > 0) {
+        let randomMove = availableSquares[Math.floor(Math.random() * availableSquares.length)];
+        return randomMove;
+    }
+    return null;
+}
+
+
+function aiTurn() {
+    let aiMove = getAiMove();
+    if (aiMove !== null) {
+        board[aiMove] = currentPlayer;
+        document.querySelectorAll('.cell')[aiMove].textContent = currentPlayer;
+        if (checkWinner() === null) {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         }
     }
+}
+
+function makeMove(square) {
+    if (typeof board[square] === 'number' && checkWinner() === null) {
+        board[square] = currentPlayer;
+        document.getElementById('message').textContent = '';
+        document.querySelectorAll('.cell')[square].textContent = currentPlayer;
+        if (checkWinner() === null) {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            aiTurn();
+        }
+    }
+}
 
 function restartGame() {
     for (let i = 0; i < board.length; i++) {
@@ -57,9 +95,6 @@ function restartGame() {
     document.getElementById('message').textContent = ''; // Clear any messages
 }
 
-
-
-
 const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
 const themeColorMetaTag = document.querySelector('meta[name=theme-color]');
@@ -68,4 +103,12 @@ darkModeToggle.addEventListener('change', () => {
     const isDarkMode = darkModeToggle.checked;
     body.classList.toggle('dark-mode', isDarkMode);
     themeColorMetaTag.content = isDarkMode ? '#000000' : '#ffffff';
+});
+
+// Add event listeners to the game cells
+document.querySelectorAll('.cell').forEach(cell => {
+    cell.addEventListener('click', () => {
+        const cellIndex = Array.from(document.querySelectorAll('.cell')).indexOf(cell);
+        makeMove(cellIndex);
+    });
 });
